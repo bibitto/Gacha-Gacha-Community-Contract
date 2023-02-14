d6827776f0d5e8c245eab3109cf4042a687d039d
bibitto <bibitto.crypto@gmail.com>
2023-02-14 14:08:45 +0900

    fix: lint error

--- contracts/interfaces/IERC998ERC721TopDownEnumerable.sol
+++ contracts/interfaces/IERC998ERC721TopDownEnumerable.sol
@@ -1,17 +1,18 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-///  @dev The ERC-165 identifier for this interface is 0xa344afe4
+//  @dev The ERC-165 identifier for this interface is 0xa344afe4
 interface IERC998ERC721TopDownEnumerable {
 
-  /// @notice Get the total number of child contracts with tokens that are owned by tokenId.
-  /// @param _tokenId The parent token of child tokens in child contracts
-  /// @return uint256 The total number of child contracts with tokens owned by tokenId.
+  // @notice Get the total number of child contracts with tokens that are owned by tokenId.
+  // @param _tokenId The parent token of child tokens in child contracts
+  // @return uint256 The total number of child contracts with tokens owned by tokenId.
   function totalChildContracts(uint256 _tokenId) external view returns(uint256);
 
-  /// @notice Get child contract by tokenId and index
-  /// @param _tokenId The parent token of child tokens in child contract
-  /// @param _index The index position of the child contract
-  /// @return childContract The contract found at the tokenId and index.
+  // @notice Get child contract by tokenId and index
+  // @param _tokenId The parent token of child tokens in child contract
+  // @param _index The index position of the child contract
+  // @return childContract The contract found at the tokenId and index.
   function childContractByIndex(
     uint256 _tokenId,
     uint256 _index
@@ -20,10 +21,10 @@ interface IERC998ERC721TopDownEnumerable {
     view
     returns (address childContract);
 
-  /// @notice Get the total number of child tokens owned by tokenId that exist in a child contract.
-  /// @param _tokenId The parent token of child tokens
-  /// @param _childContract The child contract containing the child tokens
-  /// @return uint256 The total number of child tokens found in child contract that are owned by tokenId.
+  // @notice Get the total number of child tokens owned by tokenId that exist in a child contract.
+  // @param _tokenId The parent token of child tokens
+  // @param _childContract The child contract containing the child tokens
+  // @return uint256 The total number of child tokens found in child contract that are owned by tokenId.
   function totalChildTokens(
     uint256 _tokenId,
     address _childContract
@@ -32,11 +33,11 @@ interface IERC998ERC721TopDownEnumerable {
     view
     returns(uint256);
 
-  /// @notice Get child token owned by tokenId, in child contract, at index position
-  /// @param _tokenId The parent token of the child token
-  /// @param _childContract The child contract of the child token
-  /// @param _index The index position of the child token.
-  /// @return childTokenId The child tokenId for the parent token, child token and index
+  // @notice Get child token owned by tokenId, in child contract, at index position
+  // @param _tokenId The parent token of the child token
+  // @param _childContract The child contract of the child token
+  // @param _index The index position of the child token.
+  // @return childTokenId The child tokenId for the parent token, child token and index
   function childTokenByIndex(
     uint256 _tokenId,
     address _childContract,
