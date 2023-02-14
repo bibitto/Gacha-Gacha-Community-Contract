6fcb7d61480a084354dcf2f2ab6dd3370ee3474a
bibitto <bibitto.crypto@gmail.com>
2023-02-14 14:10:39 +0900

    chore: set up

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,45 @@
+import "hardhat-contract-sizer";
+import * as dotenv from "dotenv";
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@nomiclabs/hardhat-web3";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "@nomicfoundation/hardhat-network-helpers";
+
+dotenv.config();
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+    solidity: {
+        version: "0.8.18",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 200,
+            },
+        },
+    },
+    networks: {
+        hardhat: {
+            chainId: 1337,
+            allowUnlimitedContractSize: true,
+        },
+        astar: {
+            url: process.env.ASTAR_URL || "",
+            accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        },
+        polygon: {
+            url: process.env.POLYGON_URL || "",
+            accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        },
+        mumbai: {
+            url: process.env.MUMBAI_URL || "",
+            accounts: process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+        },
+    },
 };
 
 export default config;
